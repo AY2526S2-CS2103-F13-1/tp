e5b46609adcc67c20d136b3a580da520c9734133
Teh Hock Jian <hockjianteh@gmail.com>
2026-03-21 21:12:13 +0800

    Update GamerBuilder.java to fit new attributes

--- src/test/java/seedu/blockbook/testutil/GamerBuilder.java
+++ src/test/java/seedu/blockbook/testutil/GamerBuilder.java
@@ -1,15 +1,16 @@
 package seedu.blockbook.testutil;
 
-//import java.util.HashSet;
-//import java.util.Set;
-
+import seedu.blockbook.model.gamer.Country;
+import seedu.blockbook.model.gamer.Email;
+import seedu.blockbook.model.gamer.Favourite;
 import seedu.blockbook.model.gamer.Gamer;
 import seedu.blockbook.model.gamer.GamerTag;
+import seedu.blockbook.model.gamer.Group;
 import seedu.blockbook.model.gamer.Name;
-
-//import seedu.blockbook.model.gamer.Phone;
-//import seedu.blockbook.model.gamer.Email;
-//import seedu.blockbook.model.util.SampleDataUtil;
+import seedu.blockbook.model.gamer.Note;
+import seedu.blockbook.model.gamer.Phone;
+import seedu.blockbook.model.gamer.Region;
+import seedu.blockbook.model.gamer.Server;
 
 /**
  * A utility class to help with building Gamer objects.
@@ -20,12 +21,23 @@ public class GamerBuilder {
     public static final String DEFAULT_GAMER_TAG = "Herobrine";
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
-    public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_GROUP = "Raid Team";
+    public static final String DEFAULT_SERVER = "127.0.0.1:8080";
+    public static final String DEFAULT_FAVOURITE = "fav";
+    public static final String DEFAULT_COUNTRY = "Singapore";
+    public static final String DEFAULT_REGION = "ASIA";
+    public static final String DEFAULT_NOTE = "test_note";
 
     private Name name;
     private GamerTag gamerTag;
-    // private Phone phone;
-    // private Email email;
+    private Phone phone;
+    private Email email;
+    private Group group;
+    private Server server;
+    private Favourite favourite;
+    private Country country;
+    private Region region;
+    private Note note;
 
     /**
      * Creates a {@code GamerBuilder} with the default details.
@@ -33,7 +45,14 @@ public class GamerBuilder {
     public GamerBuilder() {
         name = new Name(DEFAULT_NAME);
         gamerTag = new GamerTag(DEFAULT_GAMER_TAG);
-
+        phone = new Phone(DEFAULT_PHONE);
+        email = new Email(DEFAULT_EMAIL);
+        group = new Group(DEFAULT_GROUP);
+        server = new Server(DEFAULT_SERVER);
+        favourite = new Favourite(DEFAULT_FAVOURITE);
+        country = new Country(DEFAULT_COUNTRY);
+        region = new Region(DEFAULT_REGION);
+        note = new Note(DEFAULT_NOTE);
     }
 
     /**
@@ -42,11 +61,21 @@ public class GamerBuilder {
     public GamerBuilder(Gamer gamerToCopy) {
         name = gamerToCopy.getName();
         gamerTag = gamerToCopy.getGamerTag();
-        // email = gamerToCopy.getEmail();
+        phone = gamerToCopy.getPhone();
+        email = gamerToCopy.getEmail();
+        group = gamerToCopy.getGroup();
+        server = gamerToCopy.getServer();
+        favourite = gamerToCopy.getFavourite();
+        country = gamerToCopy.getCountry();
+        region = gamerToCopy.getRegion();
+        note = gamerToCopy.getNote();
     }
 
     /**
      * Sets the {@code Name} of the {@code Gamer} that we are building.
+     *
+     * @param name A valid name.
+     * @return This builder.
      */
     public GamerBuilder withName(String name) {
         this.name = new Name(name);
@@ -55,31 +84,105 @@ public class GamerBuilder {
 
     /**
      * Sets the {@code GamerTag} of the {@code Gamer} that we are building.
+     *
+     * @param gamerTag A valid gamertag.
+     * @return This builder.
      */
     public GamerBuilder withGamerTag(String gamerTag) {
         this.gamerTag = new GamerTag(gamerTag);
         return this;
     }
 
-    // /**
-    // * Sets the {@code Phone} of the {@code Gamer} that we are building.
-    // */
-    // public GamerBuilder withPhone(String phone) {
-    // this.phone = new Phone(phone);
-    // return this;
-    // }
-
-    // /**
-    // * Sets the {@code Email} of the {@code Gamer} that we are building.
-    // */
-    // public GamerBuilder withEmail(String email) {
-    // this.email = new Email(email);
-    // return this;
-    // }
-
-    public Gamer build() {
-        return new Gamer(name, gamerTag);
+    /**
+     * Sets the {@code Phone} of the {@code Gamer} that we are building.
+     *
+     * @param phone A valid phone number.
+     * @return This builder.
+     */
+    public GamerBuilder withPhone(String phone) {
+        this.phone = new Phone(phone);
+        return this;
     }
 
+    /**
+     * Sets the {@code Email} of the {@code Gamer} that we are building.
+     *
+     * @param email A valid email.
+     * @return This builder.
+     */
+    public GamerBuilder withEmail(String email) {
+        this.email = new Email(email);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Group} of the {@code Gamer} that we are building.
+     *
+     * @param group A valid group.
+     * @return This builder.
+     */
+    public GamerBuilder withGroup(String group) {
+        this.group = new Group(group);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Server} of the {@code Gamer} that we are building.
+     *
+     * @param server A valid server.
+     * @return This builder.
+     */
+    public GamerBuilder withServer(String server) {
+        this.server = new Server(server);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Favourite} of the {@code Gamer} that we are building.
+     *
+     * @param favourite A valid favourite status.
+     * @return This builder.
+     */
+    public GamerBuilder withFavourite(String favourite) {
+        this.favourite = new Favourite(favourite);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Country} of the {@code Gamer} that we are building.
+     *
+     * @param country A valid country.
+     * @return This builder.
+     */
+    public GamerBuilder withCountry(String country) {
+        this.country = new Country(country);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Region} of the {@code Gamer} that we are building.
+     *
+     * @param region A valid region.
+     * @return This builder.
+     */
+    public GamerBuilder withRegion(String region) {
+        this.region = new Region(region);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Note} of the {@code Gamer} that we are building.
+     *
+     * @param note A valid note.
+     * @return This builder.
+     */
+    public GamerBuilder withNote(String note) {
+        this.note = new Note(note);
+        return this;
+    }
+
+    public Gamer build() {
+        return new Gamer(name, gamerTag, phone, email, group, server, favourite, country, region, note);
+    }
 }
 
