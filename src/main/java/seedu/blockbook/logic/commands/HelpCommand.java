a7495f36511057a7f39967d00eae8279b3edc42a
Julian <Julianlimwq12@Gmail.com>
2026-03-23 22:35:14 +0800

    Make help command list all commands and their usages

--- src/main/java/seedu/blockbook/logic/commands/HelpCommand.java
+++ src/main/java/seedu/blockbook/logic/commands/HelpCommand.java
@@ -12,11 +12,22 @@ public class HelpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_MESSAGE = "The user guide can be found in the help menu above.\n" +
+            "These are the available commands:\n";
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        StringBuilder outputString = new StringBuilder();
+        outputString.append(SHOWING_HELP_MESSAGE);
+        outputString.append(MESSAGE_USAGE + "\n\n");
+        outputString.append(AddCommand.MESSAGE_USAGE + "\n\n");
+        outputString.append(EditCommand.MESSAGE_USAGE + "\n\n");
+        outputString.append(DeleteCommand.MESSAGE_USAGE + "\n\n");
+//        outputString.append(ClearCommand.MESSAGE_USAGE + "\");
+        outputString.append(FindCommand.MESSAGE_USAGE);
+//        outputString.append(ListCommand.MESSAGE_USAGE + "");
+
+        return new CommandResult(outputString.toString(), true, false);
     }
 }
 
