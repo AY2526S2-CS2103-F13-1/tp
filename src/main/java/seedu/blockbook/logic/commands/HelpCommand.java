4f7f5c7782ecd3f03a0134d13ebf9536ea887dad
Julian <Julianlimwq12@Gmail.com>
2026-03-24 10:29:11 +0800

    Show command usage instructions

--- src/main/java/seedu/blockbook/logic/commands/HelpCommand.java
+++ src/main/java/seedu/blockbook/logic/commands/HelpCommand.java
@@ -9,23 +9,26 @@ public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows command usage instructions.\n"
             + "Format: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "The user guide can be found in the help menu above.\n"
-            + "These are the available commands:\n";
+    public static final String SHOWING_HELP_MESSAGE = "The user guide can be found in the help menu above. "
+            + "These are the available commands:";
 
     @Override
     public CommandResult execute(Model model) {
         StringBuilder outputString = new StringBuilder();
-        outputString.append(SHOWING_HELP_MESSAGE);
+        outputString.append(SHOWING_HELP_MESSAGE + "\n");
         outputString.append(MESSAGE_USAGE + "\n\n");
         outputString.append(AddCommand.MESSAGE_USAGE + "\n\n");
         outputString.append(EditCommand.MESSAGE_USAGE + "\n\n");
-        outputString.append(DeleteCommand.MESSAGE_USAGE + "\n\n");
-//        outputString.append(ClearCommand.MESSAGE_USAGE + "\");
-        outputString.append(FindCommand.MESSAGE_USAGE);
-//        outputString.append(ListCommand.MESSAGE_USAGE + "");
+        outputString.append(DeleteCommand.MESSAGE_USAGE + "\n");
+        outputString.append("-------------------------------------------------------------------------------------\n");
+        outputString.append(FindCommand.MESSAGE_USAGE + "\n\n");
+        outputString.append(ListCommand.MESSAGE_USAGE + "\n");
+        outputString.append("-------------------------------------------------------------------------------------\n");
+        outputString.append(ClearCommand.MESSAGE_USAGE + "\n\n");
+        outputString.append(ExitCommand.MESSAGE_USAGE);
 
         return new CommandResult(outputString.toString(), true, false);
     }
