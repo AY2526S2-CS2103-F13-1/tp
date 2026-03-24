8a0f885cc4d6afc019b08293cdea9492b02a6c2e
TehDrink <kesterteh@gmail.com>
2026-03-24 17:15:03 +0800

    Add duplicate handling.

--- src/main/java/seedu/blockbook/logic/parser/ViewCommandParser.java
+++ src/main/java/seedu/blockbook/logic/parser/ViewCommandParser.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 import seedu.blockbook.logic.commands.ViewCommand;
 import seedu.blockbook.logic.parser.exceptions.ParseException;
 import seedu.blockbook.model.gamer.GamerTag;
-import seedu.blockbook.model.gamer.GamertagContainsKeywordsPredicate;
+import seedu.blockbook.model.gamer.GamertagContainsKeywordPredicate;
 
 /**
  * Parses input arguments and creates a new ViewCommand object
@@ -16,8 +16,9 @@ import seedu.blockbook.model.gamer.GamertagContainsKeywordsPredicate;
 public class ViewCommandParser implements Parser<ViewCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the ViewCommand
-     * and returns a ViewCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the
+     * ViewCommand and returns a ViewCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public ViewCommand parse(String args) throws ParseException {
@@ -30,15 +31,19 @@ public class ViewCommandParser implements Parser<ViewCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
         }
 
+        argMultimap.verifyNoDuplicatePrefixesFor(
+                PREFIX_GAMERTAG
+        );
+
         GamerTag gamerTag = ParserUtil.parseGamerTag(
                 argMultimap.getValue(PREFIX_GAMERTAG).get());
 
-        return new ViewCommand(new GamertagContainsKeywordsPredicate(gamerTag.toString()));
+        return new ViewCommand(new GamertagContainsKeywordPredicate(gamerTag.toString()));
     }
 
     /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
+     * Returns true if none of the prefixes contains empty {@code Optional}
+     * values in the given {@code ArgumentMultimap}.
      */
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
