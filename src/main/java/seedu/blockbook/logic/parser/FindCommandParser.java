20a25fb0caba034b9c2fff0729aa30d9968145ad
TehDrink <kesterteh@gmail.com>
2026-03-26 01:56:27 +0800

    Add error message for exceeding 50 chars in global search in find command.

--- src/main/java/seedu/blockbook/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/blockbook/logic/parser/FindCommandParser.java
@@ -39,9 +39,14 @@ public class FindCommandParser implements Parser<FindCommand> {
     public FindCommand parse(String args) throws ParseException {
 
         String trimmedArgs = args.trim();
+
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        if (trimmedArgs.length() > 50) {
+            throw new ParseException("Global search KEYWORD input cannot exceed 50 characters.");
         }
 
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
