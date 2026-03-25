cc0957ab8def066449537b05a425b265d5328319
TehDrink <kesterteh@gmail.com>
2026-03-26 01:48:54 +0800

    Fix styling in FindCommand.java.

--- src/main/java/seedu/blockbook/logic/commands/FindCommand.java
+++ src/main/java/seedu/blockbook/logic/commands/FindCommand.java
@@ -1,6 +1,7 @@
 package seedu.blockbook.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+
 import java.util.function.Predicate;
 
 import seedu.blockbook.commons.util.ToStringBuilder;
