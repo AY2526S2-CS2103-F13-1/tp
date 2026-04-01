4622698141f72d4afd507911820abe24da2b0317
Teh Hock Jian <hockjianteh@gmail.com>
2026-04-01 22:52:48 +0800

    Add \n to MESSAGE_USAGE in AddCommand.java

--- src/main/java/seedu/blockbook/logic/commands/AddCommand.java
+++ src/main/java/seedu/blockbook/logic/commands/AddCommand.java
@@ -19,12 +19,12 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a gamer to BlockBook.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a gamer to BlockBook.\n\n"
             + "Format: add gamertag/GAMERTAG "
             + "[name/NAME] [phone/PHONE] [email/EMAIL] "
             + "[group/GROUP] [server/SERVER] "
             + "[favourite/FAVOURITE] [country/COUNTRY] "
-            + "[region/REGION] [note/NOTE]\n"
+            + "[region/REGION] [note/NOTE]\n\n"
             + "Example: add gamertag/ilovesteve name/Herobrine "
             + "phone/99999 email/brine@gmail.com "
             + "group/DestroySteve server/127.0.0.1:8080 favourite/fav "
