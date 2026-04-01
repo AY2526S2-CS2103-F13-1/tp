2edaa7f6a13e46f56a75ff13391ca19391fed601
hwangjihun <e0959872@u.nus.edu>
2026-04-01 21:20:47 +0800

    Add fields used to sort the list in the output

--- src/main/java/seedu/blockbook/logic/commands/SortCommand.java
+++ src/main/java/seedu/blockbook/logic/commands/SortCommand.java
@@ -29,7 +29,7 @@ public class SortCommand extends Command {
             + "[group/] [server/] [favourite/] [country/] [region/] [note/]\n"
             + "Example: " + COMMAND_WORD + " name/ phone/";
 
-    public static final String MESSAGE_SORT_SUCCESS = "Sorted all contacts.";
+    public static final String MESSAGE_SORT_SUCCESS = "Sorted all contacts by %1$s.";
     public static final String MESSAGE_SORT_DEFAULT_SUCCESS = "Sorted all contacts by gamertag (default).";
     public static final String MESSAGE_EMPTY_LIST = "There are no contacts to sort!";
     public static final String MESSAGE_INVALID_ATTRIBUTES =
@@ -84,7 +84,9 @@ public class SortCommand extends Command {
 
         model.sortGamerList(comparator);
 
-        String successMessage = attributes.isEmpty() ? MESSAGE_SORT_DEFAULT_SUCCESS : MESSAGE_SORT_SUCCESS;
+        String successMessage = attributes.isEmpty()
+                ? MESSAGE_SORT_DEFAULT_SUCCESS
+                : String.format(MESSAGE_SORT_SUCCESS, String.join(", ", attributes));
         logger.info("Sort command executed successfully");
         return new CommandResult(successMessage);
     }
