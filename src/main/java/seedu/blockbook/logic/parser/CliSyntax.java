5172fd8b9072e3774b229728fe647d3aad8ae7a3
Teh Hock Jian <hockjianteh@gmail.com>
2026-04-05 14:59:22 +0800

    Add alias prefixrs in CliSyntax.java

--- src/main/java/seedu/blockbook/logic/parser/CliSyntax.java
+++ src/main/java/seedu/blockbook/logic/parser/CliSyntax.java
@@ -14,15 +14,22 @@ public class CliSyntax {
     // public static final Prefix PREFIX_GAMERTAG = new Prefix("gamertag/");
 
     public static final Prefix PREFIX_GAMERTAG = new Prefix("gamertag/");
+    public static final Prefix PREFIX_GAMERTAG_ALIAS = new Prefix("g/");
     public static final Prefix PREFIX_NAME = new Prefix("name/");
+    public static final Prefix PREFIX_NAME_ALIAS = new Prefix("n/");
     public static final Prefix PREFIX_PHONE = new Prefix("phone/");
+    public static final Prefix PREFIX_PHONE_ALIAS = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("email/");
+    public static final Prefix PREFIX_EMAIL_ALIAS = new Prefix("e/");
     public static final Prefix PREFIX_GROUP = new Prefix("group/");
+    public static final Prefix PREFIX_GROUP_ALIAS = new Prefix("gr/");
     public static final Prefix PREFIX_SERVER = new Prefix("server/");
+    public static final Prefix PREFIX_SERVER_ALIAS = new Prefix("s/");
     public static final Prefix PREFIX_FAVOURITE = new Prefix("favourite/");
     public static final Prefix PREFIX_COUNTRY = new Prefix("country/");
+    public static final Prefix PREFIX_COUNTRY_ALIAS = new Prefix("c/");
     public static final Prefix PREFIX_REGION = new Prefix("region/");
+    public static final Prefix PREFIX_REGION_ALIAS = new Prefix("r/");
     public static final Prefix PREFIX_NOTE = new Prefix("note/");
-
 }
 
