cde28b2ec21d9831e69472f8d5fd2e4ec569dcad
Teh Hock Jian <hockjianteh@gmail.com>
2026-04-07 22:10:45 +0800

    Add Unique

--- src/test/java/seedu/blockbook/model/gamer/UniqueGamerListTest.java
+++ src/test/java/seedu/blockbook/model/gamer/UniqueGamerListTest.java
@@ -1,178 +1,207 @@
 package seedu.blockbook.model.gamer;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static seedu.blockbook.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-//import static seedu.blockbook.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-//import static seedu.blockbook.testutil.Assert.assertThrows;
-//import static seedu.blockbook.testutil.TypicalGamers.ALICE;
-//import static seedu.blockbook.testutil.TypicalGamers.BOB;
-//
-//import java.util.Arrays;
-//import java.util.Collections;
-//import java.util.List;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.blockbook.model.gamer.exceptions.DuplicateGamerException;
-//import seedu.blockbook.model.gamer.exceptions.GamerNotFoundException;
-//import seedu.blockbook.testutil.GamerBuilder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.blockbook.testutil.Assert.assertThrows;
+import static seedu.blockbook.testutil.TypicalGamers.ALICE;
+import static seedu.blockbook.testutil.TypicalGamers.BENSON;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.blockbook.model.gamer.exceptions.DuplicateGamerException;
+import seedu.blockbook.model.gamer.exceptions.GamerNotFoundException;
+import seedu.blockbook.testutil.GamerBuilder;
 
 public class UniqueGamerListTest {
 
-//    private final UniqueGamerList UniqueGamerList = new UniqueGamerList();
-//
-//    @Test
-//    public void contains_nullGamer_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> UniqueGamerList.contains(null));
-//    }
-//
-//    @Test
-//    public void contains_GamerNotInList_returnsFalse() {
-//        assertFalse(UniqueGamerList.contains(ALICE));
-//    }
-//
-//    @Test
-//    public void contains_GamerInList_returnsTrue() {
-//        UniqueGamerList.add(ALICE);
-//        assertTrue(UniqueGamerList.contains(ALICE));
-//    }
-//
-//    @Test
-//    public void contains_GamerWithSameIdentityFieldsInList_returnsTrue() {
-//        UniqueGamerList.add(ALICE);
-//        Gamer editedAlice = new GamerBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND)
-//                .build();
-//        assertTrue(UniqueGamerList.contains(editedAlice));
-//    }
-//
-//    @Test
-//    public void add_nullGamer_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> UniqueGamerList.add(null));
-//    }
-//
-//    @Test
-//    public void add_duplicateGamer_throwsDuplicateGamerException() {
-//        UniqueGamerList.add(ALICE);
-//        assertThrows(DuplicateGamerException.class, () -> UniqueGamerList.add(ALICE));
-//    }
-//
-//    @Test
-//    public void setGamer_nullTargetGamer_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> UniqueGamerList.setGamer(null, ALICE));
-//    }
-//
-//    @Test
-//    public void setGamer_nullEditedGamer_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> UniqueGamerList.setGamer(ALICE, null));
-//    }
-//
-//    @Test
-//    public void setGamer_targetGamerNotInList_throwsGamerNotFoundException() {
-//        assertThrows(GamerNotFoundException.class, () -> UniqueGamerList.setGamer(ALICE, ALICE));
-//    }
-//
-//    @Test
-//    public void setGamer_editedGamerisSameGamer_success() {
-//        UniqueGamerList.add(ALICE);
-//        UniqueGamerList.setGamer(ALICE, ALICE);
-//        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
-//        expectedUniqueGamerList.add(ALICE);
-//        assertEquals(expectedUniqueGamerList, UniqueGamerList);
-//    }
-//
-//    @Test
-//    public void setGamer_editedGamerHasSameIdentity_success() {
-//        UniqueGamerList.add(ALICE);
-//        Gamer editedAlice = new GamerBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND)
-//                .build();
-//        UniqueGamerList.setGamer(ALICE, editedAlice);
-//        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
-//        expectedUniqueGamerList.add(editedAlice);
-//        assertEquals(expectedUniqueGamerList, UniqueGamerList);
-//    }
-//
-//    @Test
-//    public void setGamer_editedGamerHasDifferentIdentity_success() {
-//        UniqueGamerList.add(ALICE);
-//        UniqueGamerList.setGamer(ALICE, BOB);
-//        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
-//        expectedUniqueGamerList.add(BOB);
-//        assertEquals(expectedUniqueGamerList, UniqueGamerList);
-//    }
-//
-//    @Test
-//    public void setGamer_editedGamerHasNonUniqueIdentity_throwsDuplicateGamerException() {
-//        UniqueGamerList.add(ALICE);
-//        UniqueGamerList.add(BOB);
-//        assertThrows(DuplicateGamerException.class, () -> UniqueGamerList.setGamer(ALICE, BOB));
-//    }
-//
-//    @Test
-//    public void remove_nullGamer_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> UniqueGamerList.remove(null));
-//    }
-//
-//    @Test
-//    public void remove_GamerDoesNotExist_throwsGamerNotFoundException() {
-//        assertThrows(GamerNotFoundException.class, () -> UniqueGamerList.remove(ALICE));
-//    }
-//
-//    @Test
-//    public void remove_existingGamer_removesGamer() {
-//        UniqueGamerList.add(ALICE);
-//        UniqueGamerList.remove(ALICE);
-//        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
-//        assertEquals(expectedUniqueGamerList, UniqueGamerList);
-//    }
-//
-//    @Test
-//    public void setGamers_nullUniqueGamerList_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> UniqueGamerList.setGamers((UniqueGamerList) null));
-//    }
-//
-//    @Test
-//    public void setGamers_UniqueGamerList_replacesOwnListWithProvidedUniqueGamerList() {
-//        UniqueGamerList.add(ALICE);
-//        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
-//        expectedUniqueGamerList.add(BOB);
-//        UniqueGamerList.setGamers(expectedUniqueGamerList);
-//        assertEquals(expectedUniqueGamerList, UniqueGamerList);
-//    }
-//
-//    @Test
-//    public void setGamers_nullList_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> UniqueGamerList.setGamers((List<Gamer>) null));
-//    }
-//
-//    @Test
-//    public void setGamers_list_replacesOwnListWithProvidedList() {
-//        UniqueGamerList.add(ALICE);
-//        List<Gamer> GamerList = Collections.singletonList(BOB);
-//        UniqueGamerList.setGamers(GamerList);
-//        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
-//        expectedUniqueGamerList.add(BOB);
-//        assertEquals(expectedUniqueGamerList, UniqueGamerList);
-//    }
-//
-//    @Test
-//    public void setGamers_listWithDuplicateGamers_throwsDuplicateGamerException() {
-//        List<Gamer> listWithDuplicateGamers = Arrays.asList(ALICE, ALICE);
-//        assertThrows(DuplicateGamerException.class, () -> UniqueGamerList.setGamers(listWithDuplicateGamers));
-//    }
-//
-//    @Test
-//    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
-//        assertThrows(UnsupportedOperationException.class, ()
-//            -> UniqueGamerList.asUnmodifiableObservableList().remove(0));
-//    }
-//
-//    @Test
-//    public void toStringMethod() {
-//        assertEquals(UniqueGamerList.asUnmodifiableObservableList().toString(), UniqueGamerList.toString());
-//    }
+    private final UniqueGamerList uniqueGamerList = new UniqueGamerList();
+
+    @Test
+    public void contains_nullGamer_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueGamerList.contains(null));
+    }
+
+    @Test
+    public void contains_gamerNotInList_returnsFalse() {
+        assertFalse(uniqueGamerList.contains(ALICE));
+    }
+
+    @Test
+    public void contains_gamerInList_returnsTrue() {
+        uniqueGamerList.add(ALICE);
+        assertTrue(uniqueGamerList.contains(ALICE));
+    }
+
+    @Test
+    public void contains_gamerWithSameIdentityFieldsInList_returnsTrue() {
+        uniqueGamerList.add(ALICE);
+        Gamer editedAlice = new GamerBuilder(ALICE)
+                .withPhone("91234567")
+                .withEmail("alice2@example.com")
+                .withGroup("Arena Team")
+                .withServer("10.0.0.1:25565")
+                .withFavourite(false)
+                .withCountry("Malaysia")
+                .withRegion("EU")
+                .withNote("updated_note")
+                .build();
+        assertTrue(uniqueGamerList.contains(editedAlice));
+    }
+
+    @Test
+    public void add_nullGamer_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueGamerList.add(null));
+    }
+
+    @Test
+    public void add_duplicateGamer_throwsDuplicateGamerException() {
+        uniqueGamerList.add(ALICE);
+        assertThrows(DuplicateGamerException.class, () -> uniqueGamerList.add(ALICE));
+    }
+
+    @Test
+    public void setGamer_nullTargetGamer_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueGamerList.setGamer(null, ALICE));
+    }
+
+    @Test
+    public void setGamer_nullEditedGamer_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueGamerList.setGamer(ALICE, null));
+    }
+
+    @Test
+    public void setGamer_targetGamerNotInList_throwsGamerNotFoundException() {
+        assertThrows(GamerNotFoundException.class, () -> uniqueGamerList.setGamer(ALICE, ALICE));
+    }
+
+    @Test
+    public void setGamer_editedGamerIsSameGamer_success() {
+        uniqueGamerList.add(ALICE);
+        uniqueGamerList.setGamer(ALICE, ALICE);
+
+        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
+        expectedUniqueGamerList.add(ALICE);
+        assertEquals(expectedUniqueGamerList, uniqueGamerList);
+    }
+
+    @Test
+    public void setGamer_editedGamerHasSameIdentity_success() {
+        uniqueGamerList.add(ALICE);
+        Gamer editedAlice = new GamerBuilder(ALICE)
+                .withPhone("91234567")
+                .withEmail("alice2@example.com")
+                .withGroup("Arena Team")
+                .withServer("10.0.0.1:25565")
+                .withFavourite(false)
+                .withCountry("Malaysia")
+                .withRegion("EU")
+                .withNote("updated_note")
+                .build();
+
+        uniqueGamerList.setGamer(ALICE, editedAlice);
+
+        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
+        expectedUniqueGamerList.add(editedAlice);
+        assertEquals(expectedUniqueGamerList, uniqueGamerList);
+    }
+
+    @Test
+    public void setGamer_editedGamerHasDifferentIdentity_success() {
+        uniqueGamerList.add(ALICE);
+        uniqueGamerList.setGamer(ALICE, BENSON);
+
+        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
+        expectedUniqueGamerList.add(BENSON);
+        assertEquals(expectedUniqueGamerList, uniqueGamerList);
+    }
+
+    @Test
+    public void setGamer_editedGamerHasNonUniqueIdentity_throwsDuplicateGamerException() {
+        uniqueGamerList.add(ALICE);
+        uniqueGamerList.add(BENSON);
+        assertThrows(DuplicateGamerException.class, () -> uniqueGamerList.setGamer(ALICE, BENSON));
+    }
+
+    @Test
+    public void remove_nullGamer_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueGamerList.remove(null));
+    }
+
+    @Test
+    public void remove_gamerDoesNotExist_throwsGamerNotFoundException() {
+        assertThrows(GamerNotFoundException.class, () -> uniqueGamerList.remove(ALICE));
+    }
+
+    @Test
+    public void remove_existingGamer_removesGamer() {
+        uniqueGamerList.add(ALICE);
+        uniqueGamerList.remove(ALICE);
+
+        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
+        assertEquals(expectedUniqueGamerList, uniqueGamerList);
+    }
+
+    @Test
+    public void setGamers_nullUniqueGamerList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueGamerList.setGamers((UniqueGamerList) null));
+    }
+
+    @Test
+    public void setGamers_uniqueGamerList_replacesOwnListWithProvidedUniqueGamerList() {
+        uniqueGamerList.add(ALICE);
+        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
+        expectedUniqueGamerList.add(BENSON);
+
+        uniqueGamerList.setGamers(expectedUniqueGamerList);
+        assertEquals(expectedUniqueGamerList, uniqueGamerList);
+    }
+
+    @Test
+    public void setGamers_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueGamerList.setGamers((List<Gamer>) null));
+    }
+
+    @Test
+    public void setGamers_list_replacesOwnListWithProvidedList() {
+        uniqueGamerList.add(ALICE);
+        List<Gamer> gamerList = Collections.singletonList(BENSON);
+
+        uniqueGamerList.setGamers(gamerList);
+
+        UniqueGamerList expectedUniqueGamerList = new UniqueGamerList();
+        expectedUniqueGamerList.add(BENSON);
+        assertEquals(expectedUniqueGamerList, uniqueGamerList);
+    }
+
+    @Test
+    public void setGamers_listWithDuplicateGamers_throwsDuplicateGamerException() {
+        List<Gamer> listWithDuplicateGamers = Arrays.asList(ALICE, new GamerBuilder(ALICE)
+                .withPhone("91234567")
+                .withEmail("alice2@example.com")
+                .withGroup("Arena Team")
+                .withServer("10.0.0.1:25565")
+                .withFavourite(false)
+                .withCountry("Malaysia")
+                .withRegion("EU")
+                .withNote("updated_note")
+                .build());
+
+        assertThrows(DuplicateGamerException.class, () -> uniqueGamerList.setGamers(listWithDuplicateGamers));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+                -> uniqueGamerList.asUnmodifiableObservableList().remove(0));
+    }
+
+    @Test
+    public void toStringMethod() {
+        assertEquals(uniqueGamerList.asUnmodifiableObservableList().toString(), uniqueGamerList.toString());
+    }
 }
-
-
-
